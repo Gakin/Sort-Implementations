13b05840bb117379569b0b117f996cb998025aea
gakin <gakin@usc.edu>
2015-10-13 23:33:03 -0400

    InsertionSort Implemented

--- insertionsort.py
+++ insertionsort.py
@@ -1,1 +1,28 @@
+class InsertionSort:
 
+    def __init__(self):
+        pass
+	
+	def insertionSort(self,listToBeSorted):
+		innerIndex = 0
+		outerIndex = 1
+		
+		while outerIndex < len(listToBeSorted)-1:
+			
+			innerIndex = outerIndex
+			
+			while innerIndex > 0 and listToBeSorted[innerIndex-1] < listToBeSorted[innerIndex-1]:
+				value = listToBeSorted[innerIndex-1]
+				listToBeSorted[innerIndex-1] = listToBeSorted[innerIndex]
+				listToBeSorted[innerIndex] = value
+				innerIndex -= 1
+			
+			outerIndex += 1
+
+if __name__ == "__main__":
+
+    toBeSortedList = [5,3,8,2,1,9,7]
+    sorter = InsertionSort()
+    myList = sorter.insertionSort(toBeSortedList)
+    print myList
+		
