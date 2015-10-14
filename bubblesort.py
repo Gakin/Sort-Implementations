d4b425701aabdf0b56fdf6cac60ba8d67ef0fbf1
gakin <gakin@usc.edu>
2015-10-13 23:07:35 -0400

    BubbleSort Implemented

--- bubblesort.py
+++ bubblesort.py
@@ -1,1 +1,24 @@
+class BubbleSort:
 
+    def __init__(self):
+        pass
+	
+	def bubbleSort(self,listToBeSorted):
+		
+		flag = true
+		
+		while flag:
+			index = 1
+			while index < len(listToBeSorted)-1:
+				if listToBeSorted[index-1] > listToBeSorted[index]:
+					value = listToBeSorted[index]
+					listToBeSorted[index] = listToBeSorted[index-1]
+					listToBeSorted[index-1] = value
+				
+if __name__ == "__main__":
+
+    toBeSortedList = [5,3,8,2,1,9,7]
+    sorter = BubbleSort()
+    myList = sorter.bubbleSort(toBeSortedList)
+    print myList
+		
